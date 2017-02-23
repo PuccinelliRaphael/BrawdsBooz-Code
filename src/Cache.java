308c694872c323d5b06683bb0bd726c44a8e256d
PuccinelliRaphael <raphaelpuccinelli@gmail.com>
2017-02-23 20:02:07 +0100

    commit

--- src/Cache.java
+++ src/Cache.java
@@ -1,3 +1,17 @@
 
 public class Cache {
+    private int MAX_SIZE;
+    private int USED_SIZE = 0;
+
+    public void setMAX_SIZE(int size){
+        this.MAX_SIZE = size;
+    }
+
+    public void addUsed(int mb){
+        this.USED_SIZE += mb;
+    }
+
+    public void release(int mb){
+        this.USED_SIZE -= mb;
+    }
 }
