686588bf901c077bb0d5018eba8524f4d8c17cb5
PuccinelliRaphael <raphaelpuccinelli@gmail.com>
2017-02-23 20:10:49 +0100

    commit

--- src/EndPoint.java
+++ src/EndPoint.java
@@ -1,3 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class EndPoint {
+    private Map caches = new HashMap();
+
+    public void addCaches(int id, int latency){
+        this.caches.put(id, latency);
+    }
+
+    public Map getCaches(){
+        return this.caches;
+    }
 }
